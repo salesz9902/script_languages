8e3f11e186f92c00d77af8d39fc12d0880dd1f66
salesz9902 <sch.zsolt@outlook.com>
2022-03-03 09:56:06 +0100

    little modification on week3 homework

--- week3/hw/palindromIter.py
+++ week3/hw/palindromIter.py
@@ -6,15 +6,20 @@ def iterativ(text):
     while(i != -1):
         new += text[i]
         i -= 1
-    return new
+    if(new == text):
+        return True
+    else:
+        return False
 
 
 def main():
     num = 2002
     text = 'görög'
 
-    print(iterativ(str(num)))
-    print(iterativ(text))
+    print(f'{num} is palindrom' if iterativ(
+        str(num)) else f'{num} is not palindrom')
+    print(f'{text} is palindrom' if iterativ(
+        text) else f'{num} is not palindrom')
 
 
 if __name__ == '__main__':
--- week3/hw/palindromRecursive.py
+++ week3/hw/palindromRecursive.py
@@ -14,8 +14,10 @@ def main():
     num = 2002
     text = 'görög'
 
-    print(recursive(str(num)))
-    print(recursive(text))
+    print(f'{num} is palindrom' if recursive(
+        str(num)) else f'{num} is not palindrom')
+    print(f'{text} is palindrom' if recursive(
+        text) else f'{num} is not palindrom')
 
 
 if __name__ == '__main__':
